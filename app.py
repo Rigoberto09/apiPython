f857c45be54f7e95265b7a209ada98b72dba079b
Rigoberto09 <rigobertoborjas3@gmail.com>
2024-01-01 15:55:37 -0600

    Archivo principal de La api para llamar las hojas almacenadas en la carpeta template

---
+++ app.py
@@ -0,0 +1,18 @@
+# app.py
+# Desarollo de API usando Flask
+
+# crear requerimientos de [pip freeze > requirements.txt]
+# instalar todos los requermientos de la app.py [pip install -r requirements.txt]
+from flask import Flask, render_template
+
+
+
+app = Flask(__name__)
+
+# Inicio o ruta predeterminda a la carpeta de templates
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
